67b5eb4f38be9df2b690e0d83f1f7ca3a88b45a6
powercas_gamer <cas@deltapvp.net>
2023-04-03 16:22:49 +0200

    format

--- test-plugin/src/main/java/com/velocitypowered/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/com/velocitypowered/testplugin/TestPlugin.java
@@ -3,21 +3,20 @@ package com.velocitypowered.testplugin;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.plugin.Plugin;import org.slf4j.Logger;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
 
 @Plugin(
-        id = "test-plugin",
-        authors = "Velocity Contributors",
-        name = "Test Plugin",
-        version = "1.0.0"
-)
+    id = "test-plugin",
+    authors = "Velocity Contributors",
+    name = "Test Plugin",
+    version = "1.0.0")
 public class TestPlugin {
 
   @Inject Logger logger;
 
   @Subscribe
-  public void onProxyInitialization(ProxyInitializeEvent event) {
-        this.logger.info("Hello, World!");
-    }
-
+  public void onProxyInitialization(final ProxyInitializeEvent event) {
+    this.logger.info("Hello, World!");
+  }
 }
